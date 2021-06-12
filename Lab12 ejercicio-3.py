857b5602f9eb35e7fa916c0fbf07bae2f3c05664
Ruben Roque <roque.ect@gmail.com>
2021-06-12 06:51:31 -0500

    Add files via upload
    Resolución del ejercicio 3 del laboratorio 12, del instituto TECSUP de Arequipa, que incluye temas de funciones

---
+++ Lab12 ejercicio-3.py
@@ -0,0 +1,6 @@
+#Laboratorio 12 - Ejercicio 3
+def calcula_longitud():
+  cadena = input('Ingrese una expresión de texto: ')
+  tamano = len(cadena)
+  return tamano
+print('El tamaño de la expresión de texto es:',calcula_longitud())
