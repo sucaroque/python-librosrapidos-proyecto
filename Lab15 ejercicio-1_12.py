3322a77ddd01efc24dfb19992ca8dcca1ffed5b4
Ruben Roque <roque.ect@gmail.com>
2021-07-11 01:09:37 -0500

    Add files via upload

---
+++ Lab15 ejercicio-1_12.py
@@ -0,0 +1,32 @@
+azul_claro_blanco,purpura_blanco,azul_oscuro_blanco,cursiva=\
+"\033[1;97;44m","\033[1;97;45m","\033[1;97;46m","\033[3;1;30m"
+cielo,rojo,rosado,azul,verde,cyan,fin=\
+"\033[1;36m","\033[1;91m","\033[1;95m","\033[1;94m","\033[1;92m","\033[1;96m","\033[0m"
+print(cielo+"LABORATORIO 15 - EJERCICIO 1.12"+fin)
+class Aritmetica:
+  def __init__(self,aa,bb):
+     self.a=aa
+     self.b=bb
+  
+  def suma(self):
+     s=self.a+self.b
+     return s
+  def resta(self):
+     r=self.a-self.b
+     return r
+  def producto(self):
+     p=self.a*self.b
+     return p
+  def cociente(self):
+     c=self.a/self.b
+     return c
+  def residuo(self):
+     rd=self.a%self.b
+     return rd
+  def potencia(self):
+     pt=self.a**self.b
+     return pt
+edades=Aritmetica(24,19)
+print(verde+"PRODUCTO:"+fin,edades.resta())
+print(verde+"COCIENTE:"+fin,edades.cociente())
+print(verde+"RESIDUO:"+fin,edades.residuo())
