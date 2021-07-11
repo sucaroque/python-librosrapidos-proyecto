cc003a223045a143883254db9113b2566c96aa9a
Ruben Roque <roque.ect@gmail.com>
2021-07-11 01:42:44 -0500

    Add files via upload

---
+++ Lab15 ejercicio-2_5.py
@@ -0,0 +1,26 @@
+azul_claro_blanco,purpura_blanco,azul_oscuro_blanco,cursiva=\
+"\033[1;97;44m","\033[1;97;45m","\033[1;97;46m","\033[3;1;30m"
+cielo,rojo,rosado,azul,verde,cyan,fin=\
+"\033[1;36m","\033[1;91m","\033[1;95m","\033[1;94m","\033[1;92m","\033[1;96m","\033[0m"
+print(cielo+"LABORATORIO 15 - EJERCICIO 2.5"+fin)
+class Rectangulo:
+  def __init__(self,l,a):
+     self.largo=l
+     self.ancho=a
+  
+  def perimetro(self):
+     p=2*self.largo+2*self.ancho
+     return p
+  def area(self):
+     a=self.largo*self.ancho
+     return a
+  def diagonal(self):
+     d=(self.largo**2+self.ancho**2)**0.5
+     return d
+
+rect1=Rectangulo(3,4)
+print(rosado+"PERIMETRO:"+fin,rect1.perimetro())
+print(rosado+"AREA\t:"+fin,rect1.area())
+print(rosado+"DIAGONAL:"+fin,rect1.diagonal())
+print(rosado+"LARGO\t:"+fin,rect1.largo)
+print(rosado+"ANCHO\t:"+fin,rect1.ancho)
